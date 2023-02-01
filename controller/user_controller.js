06d980c274b9561482d54e6a184b7e326068278b
Rishi0814 <rishibarad007@gmail.com>
2023-02-01 17:53:11 +0530

    changed Vertification url

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -31,7 +31,7 @@ exports.signup = async (req, res) => {
 
     const verificationToken = user.generateVerificationToken();
     // console.log(verificationToken);
-    const url = `http://localhost:2000/verify/${verificationToken}`;
+    const url = `https://email-verification-ov42.onrender.com/verify/${verificationToken}`;
     // console.log(url);
 
     transporter.sendMail({
